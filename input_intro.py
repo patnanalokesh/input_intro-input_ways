d19c9f7e0aacaa642646c8ccab54ac932b450bd0
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-08-20 17:02:19 +0530

    Add files via upload

---
+++ input_intro.py
@@ -0,0 +1,21 @@
+# a=input('Enter the value for a:')
+# b=input()
+# print(type(a))
+# print(type(b))
+# print(a+b)
+
+# a=int(input('Enter your phone number : '))
+# print('The number you have entered is',a)
+
+# g=float(input('Enter your height: '))
+# print('your height is:',g)
+
+# a=int(input())
+# b=float(input())
+# print(a+b)
+
+# a=int(input())
+# b=str(input())
+# # print(a+int(b))
+# # print(str(a)+b)
+# print(a+b)
---
+++ input_ways.py
@@ -0,0 +1,80 @@
+# a=input('Enter your Name:')
+# print("My name is",a)
+# print("My name is",a,sep="-")
+# print("My name is",int(a))
+# print("My name is",float(a))
+# print("My name is"+' '+str(a))
+# print("My name is"+' '+a)
+
+
+# a=int(input('Enter your Phone Number: '))
+# print('Your Phone Number is',a)
+# print('Your Phone Number is',a,sep="-")
+# print('Your Phone Number is',int(a))
+# print('Your Phone Number is',float(a))
+# print('Your Phone Number is'+' '+str(a))
+# print('Your Phone Number is',a)
+
+# a=float(input('Enter your BMI:'))  # here BMI stands for [Body Mass Index]
+# print('Your BMI is:',a)
+# print('Your BMI is:',a,sep="-")
+# print('Your BMI is:',int(a))
+# print('Your BMI is:',float(a))
+# print('Your BMI is:'+' '+str(a))
+
+# name=input('Enter your name:')
+# age=int(input('Enter your age:'))
+# per=float(input('Enter your Graduation percentage:'))
+
+# print(name,age,per)
+# print(name,per,name,age)
+# print(age,name,age)
+# print(per)
+
+# name=input('Enter your name:')
+# age=int(input('Enter your age:'))
+# per=float(input('Enter your Graduation year:'))
+
+# print("Your name is",name,"and age is",age,\
+#     "Graduation percentage is",per)
+
+# print("your name is"+' '+name,"and age is",age,\
+#     "Graduation percentage is",str(per)+'%')
+
+# print("Your name is"+' '+name,"and age is",age,\
+#     "Graduation percentage is",str(per),"%")
+
+# print("your name is %s and age is %d.\
+#     Graduation percentage is %.2f"%(name,age,per))
+
+# print("Your name is {} and age is {}.\
+# Graduation percentage is {}".format(name,age,per))
+
+# print("your name is {1} and age is {0}.\
+# Graduation percentage is {2}".format(name,age,per))
+
+# print(f"Your name is {name} and age is {age}.\
+# Graduation percentage is {per}%")  # here 'f' stands for fast string.
+
+# a=int(input())
+# b=int(input())
+# print(a+b)
+
+# a,b=[int(x) for x in input('Enter any two numbers: ').split()]
+# print(a+b)
+
+# a=[float(x) for x in input().split(',')]
+# print(sum(a))
+
+# import math
+# a=[float(x) for x in input().split(',')]
+# print(math.prod(a))
+
+# x='12345687946'
+# print(int(x[0])+int(x[-1]))
+
+# g='89754522362555265'
+# print(int(g[0])+int(g[-3]))
+
+# f=input()
+# print(int(f[-6])+int(f[8]))
